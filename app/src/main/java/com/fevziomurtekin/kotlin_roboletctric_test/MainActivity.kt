a7b3b7ee5a7b07c83daed81fd1f9edfdc3ee3704
fevziomurtekin <fevziomurtekin@gmail.com>
2019-02-05 23:31:17 +0300

    init  🚀

---
+++ app/src/main/java/com/fevziomurtekin/kotlin_roboletctric_test/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.fevziomurtekin.kotlin_roboletctric_test
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnClick.setOnClickListener(this::changeText)
+    }
+
+
+    fun changeText(view:View){
+        text.text="Text"
+    }
+
+}
---
+++ app/src/test/java/com/fevziomurtekin/kotlin_roboletctric_test/RobolectricTest.kt
@@ -0,0 +1,65 @@
+package com.fevziomurtekin.kotlin_roboletctric_test
+
+import android.widget.Button
+import android.widget.TextView
+import org.hamcrest.CoreMatchers
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.Robolectric
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
+
+@RunWith(RobolectricTestRunner::class)
+
+class RobolectricTest{
+
+    var activity:MainActivity?=null
+    var textview:TextView?=null
+    var button:Button?=null
+
+    @Before
+    fun init(){ /*Test activityimizi oluşturuduk.*/
+        activity = Robolectric.setupActivity(MainActivity::class.java)
+        textview = activity?.findViewById(R.id.text)
+        button = activity?.findViewById(R.id.btnClick)
+    }
+
+    @Test
+    fun checkActivity(){ /*activity nin null olup olmadığını kontrol ediyoruz.*/
+        Assert.assertNotNull(activity)
+    }
+
+    @Test
+    fun checkTextView(){
+        Assert.assertNotNull(textview)
+    }
+
+    @Test
+    fun checkButton(){
+        Assert.assertNotNull(button)
+    }
+
+    @Test
+    fun checkTextView_text(){ /*Textview içerisindeki yazıyı kontrol ediyoruz.*/
+        val text = textview?.text.toString()
+        Assert.assertThat(text, CoreMatchers.equalTo("Merhaba"))
+    }
+
+
+    @Test
+    fun checkTextView_width(){ /*Textview genişliğini kontrol ediyoruz.*/
+        val width=textview?.width
+        Assert.assertTrue("Genişliği daha fazladır.", width!! >70)
+    }
+
+    @Test
+    fun checkButtonClick_text(){ /*Buttona tıklandığında değişecek texti kontrol ediyoruz.*/
+        button?.performClick()
+        val text = textview?.text.toString()
+        Assert.assertThat(text,CoreMatchers.equalTo("Text"))
+    }
+
+
+}
